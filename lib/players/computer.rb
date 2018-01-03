fdd3456f922c5b4dad993dd9c46ee231a0001f40
Brittany Morris <brittmmendez@gmail.com>
2018-01-03 22:53:14 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -31,8 +31,24 @@ module Players
       elsif board.taken?(7) && board.cells[6] == board.cells[8] && !board.taken?(8)
         move=8.to_s
 
+      #[0,3,6]
+      elsif board.taken?(1) && board.cells[0] == board.cells[3] && !board.taken?(7)
+      move=7.to_s
+    elsif board.taken?(4) && board.cells[3] == board.cells[6] && !board.taken?(1)
+      move=1.to_s
+    elsif board.taken?(1) && board.cells[0] == board.cells[6] && !board.taken?(4)
+      move=4.to_s
 
-        #still need to create [0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]
+      #[1,4,7]
+    elsif board.taken?(2) && board.cells[1] == board.cells[4] && !board.taken?(8)
+    move=8.to_s
+  elsif board.taken?(5) && board.cells[4] == board.cells[7] && !board.taken?(2)
+    move=2.to_s
+  elsif board.taken?(2) && board.cells[1] == board.cells[7] && !board.taken?(5)
+    move=5.to_s
+
+
+        #[2,5,8],[0,4,8],[6,4,2]
       else
         move=rand(1..9).to_s
 
